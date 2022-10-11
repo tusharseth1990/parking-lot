462f264dd49776cf3fd23284006aa9b09f49adef
TUSHAR SETH <tusharseth1990@gmail.com>
2022-10-11 23:09:10 +0530

    Update ParkingLotTest.java

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -91,4 +91,79 @@ public class ParkingLotTest {
 
     }
 
+    @Test
+    void parkAndUnParkBikeFromAirport(){
+        ParkingLot parkingLot27 = new ParkingLot();
+        Vehicle vehicle27 = new Vehicle(VehicleType.BIKE, "UP32-0878");
+        ParkingTicket parkingTicket27 = parkingLot27.parkVehicle(vehicle27, ParkingLocation.STADIUM);
+        parkingTicket27.setEntryDateTime(new Timestamp(System.currentTimeMillis()-1800000));
+        ParkingReceipt parkingReceipt27 = parkingLot27.unParkVehicle(parkingTicket27);
+        assertEquals(30,parkingReceipt27.getFees());
+    }
+
+    @Test
+    void parkAndUnParkBike2FromAirport(){
+        ParkingLot parkingLot25 = new ParkingLot();
+        Vehicle vehicle25 = new Vehicle(VehicleType.CAR, "UP32-1234");
+        ParkingTicket parkingTicket25 = parkingLot25.parkVehicle(vehicle25, ParkingLocation.AIRPORT);
+        System.out.println(parkingTicket25);
+        //10800000 - 3 hours
+        parkingTicket25.setEntryDateTime(new Timestamp(System.currentTimeMillis()-10800000));
+        ParkingReceipt parkingReceipt25= parkingLot25.unParkVehicle(parkingTicket25);
+        assertEquals(60,parkingReceipt25.getFees());
+    }
+
+    @Test
+    void parkAndUnParkBike3rdFromAirport(){
+        ParkingLot parkingLot26 = new ParkingLot();
+        Vehicle vehicle26 = new Vehicle(VehicleType.CAR, "UP32-7567");
+        ParkingTicket parkingTicket26 = parkingLot26.parkVehicle(vehicle26, ParkingLocation.AIRPORT);
+        //32400000- 9 hours
+        parkingTicket26.setEntryDateTime(new Timestamp(System.currentTimeMillis()-32400000));
+       ParkingReceipt parkingReceipt = parkingLot26.unParkVehicle(parkingTicket26);
+       assertEquals(60,parkingReceipt.getFees());
+    }
+
+
+    @Test
+    void parkAndUnParkBike4thFromAirport(){
+        ParkingLot parkingLot28 = new ParkingLot();
+        Vehicle vehicle28 = new Vehicle(VehicleType.CAR, "UP32-9739");
+        ParkingTicket parkingTicket28 = parkingLot28.parkVehicle(vehicle28, ParkingLocation.AIRPORT);
+        parkingTicket28.setEntryDateTime(new Timestamp(System.currentTimeMillis()-86500000));
+        ParkingReceipt parkingReceipt = parkingLot28.unParkVehicle(parkingTicket28);//         86400000- 1 day
+        assertEquals(200,parkingReceipt.getFees());
+    }
+
+    @Test
+    void parkAndUnParkBikeInStadium(){
+        ParkingLot parkingLot21 = new ParkingLot();
+        Vehicle vehicle21 = new Vehicle(VehicleType.BIKE, "UP32-1234");
+        ParkingTicket parkingTicket21 = parkingLot21.parkVehicle(vehicle21, ParkingLocation.STADIUM);
+        //32400000- 9 hours
+        parkingTicket21.setEntryDateTime(new Timestamp(System.currentTimeMillis()-32400000));
+        ParkingReceipt parkingReceipt = parkingLot21.unParkVehicle(parkingTicket21);
+        assertEquals(90,parkingReceipt.getFees());
+
+    }
+@Test
+    void parkAndUnParkBike2ndCaseInStadium(){
+        ParkingLot parkingLot23 = new ParkingLot();
+        Vehicle vehicle23 = new Vehicle(VehicleType.BIKE, "UP32-0878");
+        ParkingTicket parkingTicket23 = parkingLot23.parkVehicle(vehicle23, ParkingLocation.STADIUM);
+        parkingTicket23.setEntryDateTime(new Timestamp(System.currentTimeMillis()-86500000));
+        ParkingReceipt parkingReceipt = parkingLot23.unParkVehicle(parkingTicket23);//         86400000- 1 day
+        assertEquals(1390,parkingReceipt.getFees());
+    }
+
+    @Test
+    void parkAndUnParkBike3rdCaseInStadium(){
+        ParkingLot parkingLot24 = new ParkingLot();
+        Vehicle vehicle24 = new Vehicle(VehicleType.BIKE, "UP32-9739");
+        ParkingTicket parkingTicket24 = parkingLot24.parkVehicle(vehicle24, ParkingLocation.STADIUM);
+        //10800000 - 3 hours
+        parkingTicket24.setEntryDateTime(new Timestamp(System.currentTimeMillis()-10800000));
+        ParkingReceipt parkingReceipt = parkingLot24.unParkVehicle(parkingTicket24);
+        assertEquals(30,parkingReceipt.getFees());
+    }
 }
