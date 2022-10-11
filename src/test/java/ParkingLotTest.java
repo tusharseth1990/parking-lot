a32925bdf9020886ee5390efb8818740f9fba661
seth <tusharseth1990@gmail.com>
2022-10-11 13:39:44 +0530

    Added more test cases

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -1,3 +1,4 @@
+import DTO.ParkingReceipt;
 import DTO.ParkingTicket;
 import DTO.Vehicle;
 import enums.ParkingLocation;
@@ -6,6 +7,8 @@ import exception.ParkingLotException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import service.ParkingLot;
+
+import java.sql.Timestamp;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,7 +31,6 @@ public class ParkingLotTest {
         ParkingLot parkingLot1 = new ParkingLot();
         Vehicle vehicle1 = new Vehicle(VehicleType.BIKE, "UP78-7865");
         ParkingTicket parkingTicket1 = parkingLot1.parkVehicle(vehicle1, ParkingLocation.STADIUM);
-        //System.out.println(parkingTicket1);
         assertEquals(ParkingLocation.STADIUM, parkingTicket1.getParkingLocation());
         assertEquals(VehicleType.BIKE,parkingTicket1.getParkingSpot().getVehicleType());
         assertTrue(parkingTicket1.getParkingSpot().getSpotId()!=null);
@@ -48,4 +50,45 @@ public class ParkingLotTest {
         Vehicle vehicle1 = new Vehicle(VehicleType.CAR, "UP32-5674");
         assertThrows(ParkingLotException.class, () -> parkingLot1.parkVehicle(vehicle1, null));
     }
+
+    @Test
+    void parkAndUnParkVehicleCarFromMall(){
+        ParkingLot parkingLot2 = new ParkingLot();
+        Vehicle vehicle2 = new Vehicle(VehicleType.CAR, "UP70-9845");
+        ParkingTicket parkingTicket2 = parkingLot2.parkVehicle(vehicle2, ParkingLocation.MALL);
+        parkingTicket2.setEntryDateTime(new Timestamp(System.currentTimeMillis()-20000000));
+
+        ParkingReceipt parkingReceipt = parkingLot2.unParkVehicle(parkingTicket2);
+        assertNotNull(parkingReceipt.getParkingTicket().getTicketNumber());
+        assertNotNull(parkingReceipt.getParkingTicket().getParkingSpot().getSpotId());
+        assertEquals(parkingReceipt.getFees(),120);
+    }
+
+
+    @Test
+    void parkAndUnParkVehicleBikeFromAirport(){
+        ParkingLot parkingLot2 = new ParkingLot();
+        Vehicle vehicle2 = new Vehicle(VehicleType.BIKE, "UP32-0976");
+        ParkingTicket parkingTicket2 = parkingLot2.parkVehicle(vehicle2, ParkingLocation.AIRPORT);
+        parkingTicket2.setEntryDateTime(new Timestamp(System.currentTimeMillis()-20000000));
+
+        ParkingReceipt parkingReceipt = parkingLot2.unParkVehicle(parkingTicket2);
+        assertNotNull(parkingReceipt.getParkingTicket().getTicketNumber());
+        assertNotNull(parkingReceipt.getParkingTicket().getParkingSpot().getSpotId());
+        assertEquals(parkingReceipt.getFees(),40);
+    }
+
+    @Test
+    void parkAndUnParkVehicleBikeFromAirportNegative(){
+        ParkingLot parkingLot2 = new ParkingLot();
+        assertThrows(ParkingLotException.class, () -> parkingLot2.unParkVehicle(null));
+    }
+
+    @Test
+    void parkAndUnParkVehicleCarFromMallNegative(){
+        ParkingLot parkingLot2 = new ParkingLot();
+        assertThrows(ParkingLotException.class, () ->  parkingLot2.unParkVehicle(null));
+
+    }
+
 }
